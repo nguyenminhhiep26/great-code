e8fd85b1d420f7c1eec177f92aeeb0f830de5182
nguyenminhhiep26 <73331035+nguyenminhhiep26@users.noreply.github.com>
2021-01-26 21:55:32 +0700

    Add files via upload

---
+++ Task06/ASM2.sql
@@ -0,0 +1,112 @@
+﻿CREATE DATABASE ASM2
+GO
+USE ASM2
+GO
+--Bảng sản phẩm 
+CREATE TABLE Productes(
+ProID int,
+Name varchar(40),
+Motahang varchar(40),
+Namehang varchar(40),
+Donvi varchar(40),
+Gia money,
+SoLuong int,
+CatID int
+)
+GO
+
+INSERT INTO Productes(ProID,Name,Motahang,Namehang,Donvi,Gia,SoLuong,CatID) VALUES (123,'MAY TINH T450','MAY NHAP CU','Asus','Chiec',$1000, 10, 111)
+INSERT INTO Productes(ProID,Name,Motahang,Namehang,Donvi,Gia,SoLuong,CatID) VALUES (456,'DIEN THOAI NOKIA5670','DIEN THOAI DANG HOT','Asus','Chiec',$200, 200, 222)
+INSERT INTO Productes(ProID,Name,Motahang,Namehang,Donvi,Gia,SoLuong,CatID) VALUES (789,'MAY IN SAMSUNG450','MAY IN DANG LOAI BINH','Asus','Chiec',$100, 10, 333)
+
+GO
+
+SELECT * FROM Productes
+GO
+
+--BẢNG THỂ LOẠI
+CREATE TABLE Category(
+CatID int,
+Namehang varchar(40)
+)
+GO
+
+INSERT INTO Category(CatID,Namehang) VALUES (111, 'Asus')
+INSERT INTO Category(CatID,Namehang) VALUES (222, 'TORANO')
+INSERT INTO Category(CatID,Namehang) VALUES (333, 'OUT')
+GO 
+
+SELECT * FROM Category
+GO
+
+--BẢNG HÃNG HÀNG
+CREATE TABLE GOODS(
+GOODSID int,
+Namehang varchar(40),
+ADDRESS varchar(40),
+Tel int
+)
+GO
+
+INSERT INTO GOODS(GOODSID,Namehang,ADDRESS,Tel) VALUES (789,'Asus', 'USE', 983232)
+GO
+
+SELECT * FROM GOODS
+GO
+
+--4 .các câu lênh truy vấn để
+--a) Hiển thị tất cả các hãng sản xuất.
+SELECT Namehang FROM Category
+GO
+--b) Hiển thị tất cả các sản phẩm.
+SELECT NAME FROM Productes
+GO
+
+--5 Viết các câu lệnh truy vấn để
+--a) Liệt kê danh sách hãng theo thứ thự ngược với alphabet của tên.
+SELECT * FROM Category
+ORDER BY  Namehang DESC
+GO
+--b) Liệt kê danh sách sản phẩm của cửa hàng theo thứ thự giá giảm dần.
+SELECT * FROM Productes
+ORDER BY Name DESC
+GO
+--c) Hiển thị thông tin của hãng Asus.
+SELECT * FROM GOODS
+GO
+--d) Liệt kê danh sách sản phẩm còn ít hơn 11 chiếc trong kho
+SELECT * FROM Productes
+WHERE SoLuong < 11
+GO
+--e) Liệt kê danh sách sản phẩm của hãng Asus
+SELECT Namehang FROM Category
+GO
+
+--6 Viết các câu lệnh truy vấn để lấy
+--a) số hãng sản phẩm mà cửa hàng có
+SELECT GOODSID FROM GOODS
+GO
+--b) số mặt hàng mà cửa hàng bán 
+SELECT Name FROM Productes
+GO
+--C) Tổng số loại sản phẩm của mỗi hãng hàng có trong cửa hàng
+SELECT sum(ProID)FROM Productes
+GO
+--d) Tổng số đầu sản phẩm của toàn cửa hàng
+SELECT sum(ProID) FROM Productes
+WHERE ProID <=1
+GO
+
+--7. Thay đổi những thay đổi sau trên cơ sở dữ liệu
+--a) Viết câu lệnh để thay đổi trường giá tiền của từng mặt hàng là dương(>0)
+update Productes set Gia = $3000
+where ProID = 2
+SELECT * FROM Productes
+GO
+--b) Viết câu lệnh để thay đổi số điện thoại phải bắt đầu bằng 0.
+update GOODS set Tel = 0
+SELECT * FROM GOODS
+GO
+--c) Viết các câu lệnh để xác định các khóa ngoại và khóa chính của các bảng.
+
+
---
+++ Task06/ASM3.sql
@@ -0,0 +1,79 @@
+﻿CREATE DATABASE ASM3
+GO
+USE ASM3
+GO
+create table subscription(
+subscripId int,
+number int,
+species varchar(2423),
+date datetime
+)
+go
+
+insert into subscription(number,species,date) values (123456789,'trả trước','2000/06/29')
+insert into subscription(subscripId,number,species,date) values (2,0343619450,'trả sau','2000/06/29')
+insert into subscription(subscripId,number,species,date) values (1,123456789,'trả trước','2000/06/29')
+insert into subscription(number,species,date) values (0379784029,'trả sau','2021/01/28')
+
+go
+
+select * from subscription
+go
+
+create table customer_ok(
+customerId int,
+name nvarchar(355),
+number int,
+address nvarchar(2355)
+)
+go
+
+
+insert into customer_ok(name,number,address) values ('NGUYEN MINH HIEP ',123456789,'hà nội')
+insert into customer_ok(name,number,address) values ('TRAN THI THU',123454789,'hà nội')
+insert into customer_ok(name,number,address) values ('NGUYEN VAN HUY',123456749,'hà nội')
+insert into customer_ok(name,number,address) values ('NGUYEN TH HIEN',123456789,'hà nội')
+insert into customer_ok(name,number,address) values ('NGUYEN THI PHUONG THAO ',123456789,'hà nội')
+go
+
+select * from customer_ok
+go
+
+--4. Viết các câu lênh truy vấn để
+--A hiển thị toàn bộ thông tin khách hàng
+select * from subscription
+go
+--B hiển thị toàn bộ thông tin số thuê bao
+select * from customer_ok
+go
+
+--5. Viết các câu lệnh truy vấn để lấy
+--A hiển thị toàn bộ thông tin khách hàng có số 123456789
+select * from subscription
+where number = '123456789'
+go
+--B hiển thị toàn bộ số thuê bao có số 123456789
+select * from customer_ok 
+where number = '123456789'
+go
+--C liệt kê các thuê bao đăng kí vào ngày 29/06/2000
+select * from subscription
+where date = '2000/06/29'
+go
+--D liệt kê các thuê bao có địa chỉ hà nội
+select * from customer_ok
+where address = 'hà nội'
+go
+
+--6. Viết các câu lệnh truy vấn để lấy
+--A tổng số thuê bao của công ty là
+select sum(subscripId) from subscription
+go
+--B tổng khách hàng của công ty là
+select sum(customerId) from customer_ok
+go
+--C tổng thuê bao đăng kí ngày 2000/06/29
+select sum(customerId) from customer_ok
+go
+--7. Thay đổi những thay đổi sau trên cơ sở dữ liệu
+--a) Viết câu lệnh để thay đổi trường ngày đăng ký là not null.
